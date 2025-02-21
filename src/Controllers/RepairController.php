f9ae9ca8555d8e22093f52854c61b89faae20d4b
NoanWasTaken <delatouchenoan@gmail.com>
2025-02-21 12:15:10 +0100

    Fixed technician name and repair price

--- src/Controllers/RepairController.php
+++ src/Controllers/RepairController.php
@@ -54,7 +54,8 @@ class RepairController
                 $technician['price'] = $priceDetails['totalPrice'];
                 $technician['discount'] = $priceDetails['discount'];
                 $technician['average_rating'] = $repair->getTechnicianAverageRating($technician['technician_id']);
-                error_log("Technician Data: " . print_r($technician, true));
+                error_log("Technician Data: " . print_r($technician['price'], true));
+                error_log("Technician Data: " . print_r($priceDetails['totalPrice'], true));
             }
 
             $selectedValues = [
@@ -67,16 +68,15 @@ class RepairController
             error_log("Selected Values: " . print_r($selectedValues, true));
         }
 
-        if (isset($_GET['technician_id']) && isset($_GET['category']) && isset($_GET['address_id']) && isset($_GET['vehicle_category_id']) && isset($_GET['message'])) {
+        if (isset($_GET['technician_id']) && isset($_GET['category']) && isset($_GET['address_id']) && isset($_GET['vehicle_category_id']) && isset($_GET['message']) && isset($_GET['price'])) {
             $technician_id = $_GET['technician_id'];
             $category = $_GET['category'];
             $address_id = $_GET['address_id'];
             $vehicle_category_id = $_GET['vehicle_category_id'];
             $message = $_GET['message'];
+            $price = $_GET['price'];
 
-            $priceDetails = $this->calculatePrice($category, $vehicle_category_id, $address_id, $technician_id);
-
-            if ($repair->createRepair($user_id, $category, $address_id, $technician_id, $vehicle_category_id, $priceDetails['totalPrice'], $message)) {
+            if ($repair->createRepair($user_id, $category, $address_id, $technician_id, $vehicle_category_id, $price, $message)) {
                 $this->sendRepairCreatedEmail($user_id, $technician_id);
                 $_SESSION['success_message'] = "Votre demande a bien été envoyée !";
                 header("Location: /dashboard");
